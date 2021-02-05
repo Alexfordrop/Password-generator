f744f464493b327ff57f05789f22321918f629bc
Alex <mishechkin.aleksei@mail.ru>
2021-02-05 14:46:42 +0300

    Генератор паролей

---
+++ Gen.py
@@ -0,0 +1,21 @@
+import string
+import random
+
+LETTERS = string.ascii_letters
+NUMBERS = string.digits
+PUNCTUATION = string.punctuation
+
+def get_password_length():
+    length = input('Какой длины нужен пароль: ')
+    return int(length)
+
+def password_length(cbl, length = 8):
+    
+    printable = fetch_string_constant(cbl)
+
+    printable = list(printable)
+    random.shuffle(printable)
+
+    random_password = random.choices(printable, k=length)
+    random_password = ''.join(random_password)
+    return random_password
